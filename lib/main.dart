8bc6e484f4866a5b4c68f38f833f1a111ce9dcc3
powerninja <asdyuy23@gmail.com>
2024-02-06 22:13:58 +0900

    イメージ作成

--- lib/main.dart
+++ lib/main.dart
@@ -43,26 +43,33 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('じゃんけん✊'),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Text(
+              '相手',
+              style: TextStyle(fontSize: 30),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              '✌️',
+              style: TextStyle(fontSize: 100),
+            ),
+            SizedBox(
+              height: 80,
+            ),
+            Text(
+              '自分',
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              '👊',
+              style: TextStyle(fontSize: 200),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
