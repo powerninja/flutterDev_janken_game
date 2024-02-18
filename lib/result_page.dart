a038477c405d73ab4cd8227c629015a4facb0259
powerninja <asdyuy23@gmail.com>
2024-02-18 11:37:35 +0900

    ヘルパークラスを呼び出し、insertとgetを行うように変更

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'DBHelper.dart';
 
-class resultPage extends StatelessWidget {
-  //イニシャライザ(コンストラクタ)を記述する
-  resultPage({super.key});
+class ResultPage extends StatelessWidget {
+  ResultPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('じゃんけん✊'),
+        title: Text('結果表示'),
       ),
-      body: Container(
-        child: Text('結果表示'),
+      body: FutureBuilder<List<Memo>>(
+        future: Memo.getMemos(), // DBHelperから非同期でメモを取得
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // データの取得中に表示するウィジェット
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            // エラーが発生した場合に表示するウィジェット
+            return Center(child: Text('エラーが発生しました'));
+          } else if (snapshot.hasData) {
+            // データが正常に取得できた場合に表示するウィジェット
+            List<Memo> memos = snapshot.data!;
+            return ListView.builder(
+              itemCount: memos.length,
+              itemBuilder: (context, index) {
+                Memo memo = memos[index];
+                return ListTile(
+                  title: Text('連勝数：' + memo.rpsConsecutiveWins.toString()),
+                  // その他メモの詳細を表示
+                );
+              },
+            );
+          } else {
+            // データが空の場合に表示するウィジェット
+            return Center(child: Text('データがありません'));
+          }
+        },
       ),
     );
   }
