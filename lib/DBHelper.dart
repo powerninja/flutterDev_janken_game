00437bf474e833713a5e9792f74b5518b06a31fe
powerninja <asdyuy23@gmail.com>
2024-02-18 11:37:04 +0900

    ヘルパークラスを作成

--- lib/DBHelper.dart
+++ lib/DBHelper.dart
@@ -1,37 +1,58 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-class DBHelper {
-  static Database? _database;
+//sqflite
+class Memo {
+  final int id;
+  final String text;
+  final int rpsConsecutiveWins;
 
-  static Future<void> initDb() async {
-    if (_database != null) return;
-    try {
-      String path = join(await getDatabasesPath(), 'app_data.db');
-      _database = await openDatabase(
-        path,
-        version: 1,
-        onCreate: (db, version) {
-          return db.execute(
-            "CREATE TABLE memos(id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)",
-          );
-        },
-      );
-    } catch (e) {
-      print(e);
-    }
+//データモデルの定義
+  Memo(
+      {required this.id, required this.text, required this.rpsConsecutiveWins});
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'text': text, 'rpsConsecutiveWins': rpsConsecutiveWins};
   }
 
-  static Future<List<Map<String, dynamic>>> getMemos() async {
-    final List<Map<String, dynamic>> maps = await _database!.query('memos');
-    return maps;
+//テーブル作成
+  static Future<Database> get database async {
+    // openDatabase() データベースに接続
+    final Future<Database> database = openDatabase(
+      // getDatabasesPath() データベースファイルを保存するパス取得
+      join(await getDatabasesPath(), 'memo_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          // テーブルの作成
+          "CREATE TABLE memo(id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, rpsConsecutiveWins INTEGER)",
+        );
+      },
+      version: 2,
+    );
+    return database;
   }
 
-  static Future<void> insertMemo(Map<String, dynamic> data) async {
-    await _database!.insert(
-      'memos',
-      data,
+//データ挿入
+  static Future<void> insertMemo(Memo memo) async {
+    final Database db = await database;
+    await db.insert(
+      'memo',
+      memo.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
+  }
+
+  //データの取得
+  static Future<List<Memo>> getMemos() async {
+    final Database db = await database;
+    final List<Map<String, dynamic>> maps =
+        await db.query('memo', orderBy: 'rpsConsecutiveWins DESC');
+    return List.generate(maps.length, (i) {
+      return Memo(
+        id: maps[i]['id'],
+        text: maps[i]['text'],
+        rpsConsecutiveWins: maps[i]['rpsConsecutiveWins'],
+      );
+    });
   }
 }
